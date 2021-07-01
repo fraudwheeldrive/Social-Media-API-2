06f83325c4037e29748867326948b5fec6cf91e7
[Andrew Core] <[adcore89@gmail.com]>
2021-07-01 18:26:15 -0400

    first commit

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+const {
+    getAllThought,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+
+router
+.route('/')
+.get(getAllThought)
+.post(addThought)
+
+router
+.route('/:thoughtID')
+.get(getThoughtById)
+.put(updateThought)
+.delete(removeThought)
+
+router
+.route('/:thoughtId/reactions')
+.post(addReaction)
+
+router
+.route('/:thoughtId/reactions/:reactionID')
+.delete(removeReaction)
+
+module.exports = router; 
